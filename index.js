c407d0a9aabd1570df45a346a1581f89f187dcd5
JayArya <spice.arya@gmail.com>
2022-06-20 14:41:18 +0530

    Complete the MVP Implement remove books Implement update status

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 let userLibrary = [];
+let rowCounter = 0;
 const booksTable = document.getElementById("books-table");
 const addBookForm = document.getElementById("add-book-form");
 
@@ -11,7 +12,6 @@ addBookForm.addEventListener(
       document.getElementById("book-pages").value,
       document.getElementById("book-status-read").checked ? true : false
     );
-    addBookRow(userLibrary[userLibrary.length - 1]);
     addBookForm.reset();
     e.preventDefault();
   },
@@ -28,27 +28,49 @@ function Book(title, author, numberOfPages, haveRead) {
 addToLibrary = (title, author, numberOfPages, haveRead) => {
   const newBook = new Book(title, author, numberOfPages, haveRead);
   userLibrary.push(newBook);
+  addBookRow(newBook);
 };
 
-showBooks = () => {
-  for (let rowItr = 0; rowItr < userLibrary.length; rowItr++) {
-    let row = booksTable.insertRow(rowItr + 1);
-    row.insertCell(0).innerHTML = userLibrary[rowItr].title;
-    row.insertCell(1).innerHTML = userLibrary[rowItr].author;
-    row.insertCell(2).innerHTML = userLibrary[rowItr].numberOfPages;
-    row.insertCell(3).innerHTML = userLibrary[rowItr].haveRead
-      ? "Read"
-      : "Not Read";
-  }
-};
+// showBooks = () => {
+//   for (let rowItr = 0; rowItr < userLibrary.length; rowItr++) {
+//     let row = booksTable.insertRow(rowItr + 1);
+//     row.insertCell(0).innerHTML = userLibrary[rowItr].title;
+//     row.insertCell(1).innerHTML = userLibrary[rowItr].author;
+//     row.insertCell(2).innerHTML = userLibrary[rowItr].numberOfPages;
+//     row.insertCell(3).innerHTML = userLibrary[rowItr].haveRead
+//       ? "Read"
+//       : "Not Read";
+//   }
+// };
 
 addBookRow = ({ title, author, numberOfPages, haveRead }) => {
-  let row = booksTable.insertRow(userLibrary.length);
+  let rowNum = rowCounter;
+  const deleteBookButton = document.createElement("button");
+  deleteBookButton.innerText = "Delete";
+  // deleteBookButton.id = "deleteButton";
+  deleteBookButton.addEventListener("click", () => {
+    userLibrary[rowNum] = null;
+    document.querySelector(`[data-row-num="${rowNum}"]`).remove();
+  });
+
+  const bookStatusToggle = document.createElement("button");
+  bookStatusToggle.innerText = haveRead ? "Read" : "Pending";
+  bookStatusToggle.addEventListener("click", () => {
+    userLibrary[rowNum].haveRead = !userLibrary[rowNum].haveRead;
+    bookStatusToggle.innerText = userLibrary[rowNum].haveRead
+      ? "Read"
+      : "Pending";
+  });
+
+  let row = booksTable.insertRow();
   row.insertCell(0).innerHTML = title;
   row.insertCell(1).innerHTML = author;
   row.insertCell(2).innerHTML = numberOfPages;
-  row.insertCell(3).innerHTML = haveRead ? "Read" : "Not Read";
+  // row.insertCell(3).innerHTML = haveRead ? "Read" : "Not Read";
+  row.insertCell(3).appendChild(bookStatusToggle);
+  row.insertCell(4).appendChild(deleteBookButton);
+  row.dataset.rowNum = rowNum;
+  rowCounter++;
 };
 
 addToLibrary("The Hobbit", "J.R.R. Tolkien", "295", true);
-showBooks(userLibrary);
